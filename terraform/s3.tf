f40b7a5f12272fbc210b979737fdd787eb68701a
Adam Slack <adamslack@outlook.com>
2020-11-28 09:04:54 +0000

    redirecting errors to index

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -5,7 +5,7 @@ resource "aws_s3_bucket" "hapax" {
 
     website {
         index_document = "index.html"
-        error_document = "error.html"
+        error_document = "index.html"
     }
 
     force_destroy = false
