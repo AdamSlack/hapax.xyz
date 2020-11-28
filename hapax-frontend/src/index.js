52b7a12dad85548f04f353521af76b9bda86c36f
Adam Slack <adamslack@outlook.com>
2020-11-28 09:03:16 +0000

    redirecting errors to index

--- hapax-frontend/src/index.js
+++ hapax-frontend/src/index.js
@@ -3,24 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { createBrowserHistory } from 'history';
 
-const history = createBrowserHistory();
-
-let app = document.getElementById('root');
-if (app) {
-	const path = (/#!(\/.*)$/.exec(window.location.hash) || [])[1];
-	if (path) {
-		history.replace(path);
-	}
-
-  ReactDOM.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-    document.getElementById('root')
-  );
-}
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
