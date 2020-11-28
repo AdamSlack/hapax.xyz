d16f61eaffc72eaa9f4b247fd8d8fdf9d204e8a2
Adam Slack <adamslack@outlook.com>
2020-11-28 20:26:57 +0000

    code with line numbers, colour inversion

--- hapax-frontend/src/components/CodeBlock/index.jsx
+++ hapax-frontend/src/components/CodeBlock/index.jsx
@@ -1,25 +1,27 @@
-import React, { PureComponent } from "react";
-import PropTypes from "prop-types";
+import React from "react";
 import SyntaxHighlighter from 'react-syntax-highlighter';
-import { a11yLight  as style } from 'react-syntax-highlighter/dist/esm/styles/hljs';
-class CodeBlock extends PureComponent {
-  static propTypes = {
-    value: PropTypes.string.isRequired,
-    language: PropTypes.string
-  };
+import { dracula  as style } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 
-  static defaultProps = {
-    language: null
-  };
-
-  render() {
-    const { language, value } = this.props;
-    return (
+const CodeBlock = (props) => {
+  const { language, value } = props;
+  const numberOfLines = value.split('\n').length
+  const lineNumbers = Array.from(
+    { length: numberOfLines }, 
+    (_, idx) => idx + 1,
+  )
+  console.log(value)
+  return (
+    <div className="codeContainer">
+      <div>
+        <ul className="codeLineNumbers">
+          {lineNumbers.map((lineNumber) => <li>{lineNumber}</li>)}
+        </ul>
+      </div>
       <SyntaxHighlighter language={language} style={style}>
         {value}
       </SyntaxHighlighter>
-    );
-  }
+    </div>
+  );
 }
 
 export default CodeBlock;
