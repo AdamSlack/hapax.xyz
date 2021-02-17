e2d824615f274db557150042be3fe89967c33585
Adam Slack <adamslack@outlook.com>
2021-02-17 11:23:24 +0000

    Update index.jsx

--- hapax-frontend/src/components/HomePage/index.jsx
+++ hapax-frontend/src/components/HomePage/index.jsx
@@ -4,6 +4,7 @@ export const HomePage = () => {
   return (
     <div>
       <h2>Home Page</h2>
+      <p>Personal site used to mess about with stuff, currently used to mess a bout with displaying markdown in a React App</p>
     </div>
   )
 }
